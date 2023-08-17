1b1bcd0ccf3083a2bcfe35d7dc305e66693f3a77
Aleksandr Vasilenko <phenomen@pm.me>
2023-08-17 15:06:02 -0500

    Update header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./templates/**/*.hbs'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Inter', ...defaultTheme.fontFamily.sans],
-				serif: ['Lora', ...defaultTheme.fontFamily.serif]
-			},
-			colors: {
-				ww: {
-					orchid: '#dbd3dc',
-					purple: '#49244f',
-					silver: '#bcbec0'
-				}
-			}
-		}
-	},
-	plugins: []
+  content: ["./templates/**/*.hbs"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        serif: ["Lora", ...defaultTheme.fontFamily.serif],
+        cursive: ["Forum", ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        ww: {
+          orchid: "#dbd3dc",
+          purple: "#49244f",
+          silver: "#bcbec0",
+        },
+      },
+    },
+  },
+  plugins: [],
 };
